1c047740f7b2ecae35eca81a80ee94be91565106
Alexander Toscano Ricardo <atoscano@correo.unicordoba.edu.co>
2024-06-24 06:12:07 -0500

    Fix error uppercase U

--- src/resources/resources.controller.ts
+++ src/resources/resources.controller.ts
@@ -1,106 +1,126 @@
-import { Controller, Get, Post, Put, Patch, Delete, Body, Param, Query, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  Query,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
 import { ResourcesService } from './resources.service';
 import { CreateResourceDTO } from './dto/createResource.dto';
 import { UpdateResourceDTO } from './dto/updateResource.dto';
 import { GetResourceDTO } from './dto/getResource.dto';
 import { MongoIdPipe } from './resources.pipe';
-import {Query as ExpressQuery} from 'express-serve-static-core';
+import { Query as ExpressQuery } from 'express-serve-static-core';
 import { AuthGuard } from '@nestjs/passport';
-
 
 @Controller('resources')
 export class ResourcesController {
-    constructor(private resourcesService: ResourcesService){}
+  constructor(private resourcesService: ResourcesService) {}
 
-    @Get()
-    // @UseGuards(AuthGuard()) // Uncomment this to enable authentication
-    async getAll(@Query() query: ExpressQuery):Promise<GetResourceDTO[]>{
-        const resources = await this.resourcesService.getAll(query);
-        
-        // Map the resources to ResourceResponseDTO
-        const response:  GetResourceDTO[] = resources.map(resource => ({
-            name: resource.name,
-            description: resource.description,
-            path: resource.path,
-            type: resource.type,
-            created_by: resource.created_by,
-            updated_at: resource.updated_at,
-        }));
-        
-        return response
-    }    
+  @Get()
+  // @UseGuards(AuthGuard()) // Uncomment this to enable authentication
+  async getAll(@Query() query: ExpressQuery): Promise<GetResourceDTO[]> {
+    const resources = await this.resourcesService.getAll(query);
 
-    @Get(':id')
-    // @UseGuards(AuthGuard()) // Uncomment this to enable authentication
-    async getOne(@Param('id', MongoIdPipe) id: string ):Promise<GetResourceDTO> {
-        const resource = await this.resourcesService.getOne(id);
+    // Map the resources to ResourceResponseDTO
+    const response: GetResourceDTO[] = resources.map((resource) => ({
+      name: resource.name,
+      description: resource.description,
+      path: resource.path,
+      type: resource.type,
+      created_by: resource.created_by,
+      updated_at: resource.updated_at,
+    }));
 
-        const response: GetResourceDTO = {
-            name: resource.name,
-            description: resource.description,
-            path: resource.path,
-            type: resource.type,
-            created_by: resource.created_by,
-            updated_at: resource.updated_at,
-        }
+    return response;
+  }
 
-        return response;
-    }
-    
-    @Post()
-    @UseGuards(AuthGuard())
-    async create(@Body() newResource: CreateResourceDTO, @Req() req): Promise<GetResourceDTO>{
-        const resource = await this.resourcesService.create(newResource, req.user);
+  @Get(':id')
+  // @UseGuards(AuthGuard()) // Uncomment this to enable authentication
+  async getOne(@Param('id', MongoIdPipe) id: string): Promise<GetResourceDTO> {
+    const resource = await this.resourcesService.getOne(id);
 
-        const response: GetResourceDTO = {
-            name: resource.name,
-            description: resource.description,
-            path: resource.path,
-            type: resource.type,
-            created_by: resource.created_by,
-            updated_at: resource.updated_at,
-        }
+    const response: GetResourceDTO = {
+      name: resource.name,
+      description: resource.description,
+      path: resource.path,
+      type: resource.type,
+      created_by: resource.created_by,
+      updated_at: resource.updated_at,
+    };
 
-        return response;
-    }
+    return response;
+  }
 
-    @Patch(':id')
-    @UseGuards(AuthGuard())
-    async update(@Param('id', MongoIdPipe) id: string, @Body() updateResource: UpdateResourceDTO):Promise<GetResourceDTO>{
-        const resource = await this.resourcesService.updateById(id, updateResource);
+  @Post()
+  @UseGuards(AuthGuard())
+  async create(
+    @Body() newResource: CreateResourceDTO,
+    @Req() req,
+  ): Promise<GetResourceDTO> {
+    const resource = await this.resourcesService.create(newResource, req.user);
 
-        const response: GetResourceDTO = {
-            name: resource.name,
-            description: resource.description,
-            path: resource.path,
-            type: resource.type,
-            created_by: resource.created_by,
-            updated_at: resource.updated_at,
-        }
+    const response: GetResourceDTO = {
+      name: resource.name,
+      description: resource.description,
+      path: resource.path,
+      type: resource.type,
+      created_by: resource.created_by,
+      updated_at: resource.updated_at,
+    };
 
-        return response;
-    }
+    return response;
+  }
 
-    @Put(':id')
-    @UseGuards(AuthGuard())
-    async updatePut(@Param('id', MongoIdPipe) id: string, @Body() updateResource: UpdateResourceDTO):Promise<GetResourceDTO>{
-        const resource = await this.resourcesService.updateById(id, updateResource);
+  @Patch(':id')
+  @UseGuards(AuthGuard())
+  async update(
+    @Param('id', MongoIdPipe) id: string,
+    @Body() updateResource: UpdateResourceDTO,
+  ): Promise<GetResourceDTO> {
+    const resource = await this.resourcesService.updateById(id, updateResource);
 
-        const response: GetResourceDTO = {
-            name: resource.name,
-            description: resource.description,
-            path: resource.path,
-            type: resource.type,
-            created_by: resource.created_by,
-            updated_at: resource.updated_at,
-        }
+    const response: GetResourceDTO = {
+      name: resource.name,
+      description: resource.description,
+      path: resource.path,
+      type: resource.type,
+      created_by: resource.created_by,
+      updated_at: resource.updated_at,
+    };
 
-        return response;
-    }
+    return response;
+  }
 
-    @Delete(':id')
-    @UseGuards(AuthGuard())
-    delete(@Param('id', MongoIdPipe) id: string){
-        return this.resourcesService.delete(id);
-    }
+  @Put(':id')
+  @UseGuards(AuthGuard())
+  async updatePut(
+    @Param('id', MongoIdPipe) id: string,
+    @Body() updateResource: UpdateResourceDTO,
+  ): Promise<GetResourceDTO> {
+    const resource = await this.resourcesService.updateById(id, updateResource);
+
+    const response: GetResourceDTO = {
+      name: resource.name,
+      description: resource.description,
+      path: resource.path,
+      type: resource.type,
+      created_by: resource.created_by,
+      updated_at: resource.updated_at,
+    };
+
+    return response;
+  }
+
+  @Delete(':id')
+  @UseGuards(AuthGuard())
+  delete(@Param('id', MongoIdPipe) id: string) {
+    return this.resourcesService.delete(id);
+  }
 }
